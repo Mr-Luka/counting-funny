0b2596c5c0feb55fd84d31ad131563cda67a5683
Luka <luka.luka.luka93@gmail.com>
2024-05-02 15:07:28 -0700

    fixing up the css and made the buttons work

--- js.js
+++ js.js
@@ -1,19 +1,30 @@
 const liButtons = document.querySelectorAll("li");
-
+const nav = document.querySelector("nav");
 const soloButton = document.querySelector(".solo-button");
+const modalOuter = document.querySelector(".modal-outer");
+const modalInner = document.querySelector(".modal-inner");
 
 
-function handleSolo (e){
-    const button = e.currentTarget;
-    console.log(button)
+function handleSolo (){
+    modalOuter.classList.add("open");
+    nav.classList.add("closed")
+    if(modalOuter === "open") {
+        window.addEventListener("click", function(){
+            modalOuter.classList.remove("open")
+        })
+    }
 }
 
 soloButton.addEventListener("click", handleSolo);
 
 
-function handleLiClick(e){
-    const button2 = e.currentTarget;
-    console.log(button2)
+
+
+
+
+
+function handleLiClick() {
+    modalOuter.classList.add("open");
 }
 
 liButtons.forEach(button=>{
